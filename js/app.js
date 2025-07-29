50cd99c7955b9a6c87a2d2f613bacd4b11eccdfe
dereklwh <derek141772@gmail.com>
2025-07-28 17:26:47 -0700

    start on new website

--- js/app.js
+++ js/app.js
@@ -11,3 +11,24 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden, .hidden2');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("prefers-reduced-motion").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller-inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", "true");
+      scrollerInner.appendChild(duplicatedItem);
+    })
+  })
+}
