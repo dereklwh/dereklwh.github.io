618a49e1402f19135ef1ed6d69953ff38109f43e
dereklwh <derek141772@gmail.com>
2025-08-29 22:41:48 -0700

    imporve formatting of experience section and include logos

--- derek-website/src/components/experience-card.jsx
+++ derek-website/src/components/experience-card.jsx
@@ -1,18 +1,27 @@
+import { LuLeaf } from "react-icons/lu";
+
 export default function ExperienceCard({experiences}) {
 
     return (
         <div className='px-6 py-4 bg-white shadow-lg rounded-lg'>
             {experiences.map((experience, index) => (
-                <div key={index} className="flex items-center justify-center gap-10">
-                    <div className='flex flex-col items-center'> 
+                <div key={index} className="grid grid-cols-[1fr_auto_1fr] items-stretch mx-auto gap-10">
+                    <div className='flex flex-col items-center mt-8'> 
                         <img
                         src={experience.src}
-                        className='h-20 w-full'
+                        className='h-25 w-25 object-cover mb-6'
                         />
-                        <p className='font-bold text-base'>{experience.company}</p>
-                        <p className='text-sm text-[#92ACA0]'>{experience.location}</p>
+                        <p className='font-bold text-sm mb-2'>{experience.company}</p>
+                        <p className='text-xs text-[#92ACA0]'>{experience.location}</p>
                     </div>
-                    <div className='flex flex-col text-sm items-center'>
+                    {/* middle line */}
+                    <div className="relative flex items-center">
+                        {/* Line */}
+                        <div className="h-full border-l-2 border-dashed border-green-600"></div>
+                        {/* Leaf in center */}
+                        <LuLeaf className="absolute left-1/2 transform -translate-x-1/2 text-green-600 bg-white p-1 rounded-full" size={20} />
+                    </div>
+                    <div className='flex flex-col text-sm items-center px-2 justify-center whitespace-normal'>
                         <p>{experience.title}</p>
                         <p>{experience.time}</p>
                         <p>{experience.desc}</p>
--- derek-website/src/components/experience-section.jsx
+++ derek-website/src/components/experience-section.jsx
@@ -1,11 +1,16 @@
 import ExperienceCard from './experience-card.jsx'
 import bpLogo from '../assets/blueprint-logo.svg';
-import coffeeImage from '../assets/coffee.jpg';
+import CanucksLogo from '../assets/canucks-logo.png'
+import dwLogo from '../assets/dwave-logo.jpeg'
 
 const ExperienceSection = () => {
     const experiences = [
         {src: bpLogo, company: 'SFU Blueprint', location: 'Burnaby, BC', title: 'Software Developer', time: 'Feb 2024-Present', 
-    desc: '- Working extensively with client to successfully fulfill requirements\n- Doing else' } // Blueprint
+        desc: '- Working extensively with client to successfully fulfill requirements\n- Doing else' },
+        {src: CanucksLogo, company: 'SFU Blueprint', location: 'Burnaby, BC', title: 'Software Developer', time: 'Feb 2024-Present', 
+        desc: '- Working extensively with client to successfully fulfill requirements\n- Doing else' },  // Blueprint
+        {src: dwLogo, company: 'SFU Blueprint', location: 'Burnaby, BC', title: 'Software Developer', time: 'Feb 2024-Present', 
+        desc: '- Working extensively with client to successfully fulfill requirements\n- Doing else' }  // Blueprint
     //     {src: aaa, company: bbb, location:xxx, title:nnn, time:yyy, desc:zzz}, // Canucks
     //     {src: aaa, company: bbb, location:xxx, title:nnn, time:yyy, desc:zzz}, // D-Wave
     ]
